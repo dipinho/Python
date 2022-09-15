b91da75d475ab7cfa28e17b54503da9f716ee4e9
George de Pinho <georgedipinho@hotmail.com>
2022-09-15 16:43:16 -0300

    condicionais

---
+++ 02 - Estruturas condicionais /Intro.py
@@ -0,0 +1,5 @@
+A estética do código
+Identar um código é uma forma de manter o código fonte mais legível e manutenível. Mas em Python ela exerce um segundo papel, através da identação o interpretador consegue determinar onde um bloco de comando inicia e onde ele termina.
+
+Bloco de comando
+As linguagens de programação costumam utilizar caracteres ou palavras reservadas para terminar o inicio e fim do bloco. Em Java e C por exemplo, é utilizado chaves.
